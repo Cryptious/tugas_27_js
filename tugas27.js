82053bb729078059b7c10ffbc62f4283d2c3a991
Martino Wagey <martinowagey@gmail.com>
2019-07-22 16:57:17 +0700

    add tugas 27 JS

---
+++ tugas27.js
@@ -0,0 +1,23 @@
+function tugas27A() {
+
+  var a = "No";
+  var b = "rumah saya";
+  var c = 30;
+  var d = "A"
+
+  gabunga = a + " " + b + " " + c + d;
+
+  console.log(gabunga);
+
+}
+tugas27A();
+
+function tugas27B() {
+  var a = "Jalan pangeran Tirtayasa No";
+  var b = 70;
+
+  gabunga = a + " " + b;
+
+  console.log(gabunga);
+}
+tugas27B();
